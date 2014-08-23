26ccfe876292a4f3ee891f3fdc5f7c3aa20380b1
Alex Nisnevich <alex.nisnevich@gmail.com>
2014-08-23 16:35:27 -0700

    moving game out of /example/

---
+++ js/utils.js
@@ -0,0 +1,74 @@
+function calculateIntersection(rect1, rect2, x, y)
+{
+  // prevent x|y from being null||undefined
+  x = x || 0; y = y || 0;
+
+  // first we have to calculate the
+  // center of each rectangle and half of
+  // width and height
+  var dx, dy, r1={}, r2={};
+  r1.cx = rect1.x+x+(r1.hw = (rect1.width /2));
+  r1.cy = rect1.y+y+(r1.hh = (rect1.height/2));
+  r2.cx = rect2.x + (r2.hw = (rect2.width /2));
+  r2.cy = rect2.y + (r2.hh = (rect2.height/2));
+
+  dx = Math.abs(r1.cx-r2.cx) - (r1.hw + r2.hw);
+  dy = Math.abs(r1.cy-r2.cy) - (r1.hh + r2.hh);
+
+  if (dx < 0 && dy < 0) {
+    return {width:-dx,height:-dy};
+  } else {
+    return null;
+  }
+}
+
+/*
+ * Calculated the boundaries of an object.
+ *
+ * CAUTION: <rotation> OR <skew> attributes are NOT used for this calculation!
+ *
+ * @method getBounds
+ * @param {DisplayObject} the object to calculate the bounds from
+ * @return {Rectangle} The rectangle describing the bounds of the object
+ */
+function getBounds(obj) {
+  var bounds={x:Infinity,y:Infinity,width:0,height:0};
+
+  if ( obj instanceof createjs.Container ) {
+    var children = object.children, l=children.length, cbounds, c;
+    for ( c = 0; c < l; c++ ) {
+      cbounds = getBounds(children[c]);
+      if ( cbounds.x < bounds.x ) bounds.x = cbounds.x;
+      if ( cbounds.y < bounds.y ) bounds.y = cbounds.y;
+      if ( cbounds.width > bounds.width ) bounds.width = cbounds.width;
+      if ( cbounds.height > bounds.height ) bounds.height = cbounds.height;
+    }
+  } else {
+    var gp,imgr;
+    if ( obj instanceof createjs.Bitmap ) {
+      gp = obj.localToGlobal(0,0);
+      imgr = {width:obj.image.width,height:obj.image.height};
+    } else if ( obj instanceof createjs.BitmapAnimation ) {
+      gp = obj.localToGlobal(0,0);
+      imgr = obj.spriteSheet._frames[obj.currentFrame];
+    } else {
+      return bounds;
+    }
+
+    bounds.width = imgr.width * Math.abs(obj.scaleX);
+    if ( obj.scaleX >= 0 ) {
+      bounds.x = gp.x;
+    } else {
+      bounds.x = gp.x - bounds.width;
+    }
+
+    bounds.height = imgr.height * Math.abs(obj.scaleY);
+    if ( obj.scaleX >= 0 ) {
+      bounds.y = gp.y;
+    } else {
+      bounds.y = gp.y - bounds.height;
+    }
+  }
+
+  return bounds;
+}
