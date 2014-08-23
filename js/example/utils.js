4705a38a05f7de3df05017ebcceb9f5fe9befb90
Alex Nisnevich <alex.nisnevich@gmail.com>
2014-08-23 15:33:02 -0700

    player can move left and right

--- js/example/utils.js
+++ js/example/utils.js
@@ -72,23 +72,3 @@ function getBounds(obj) {
 
   return bounds;
 }
-
-function getWidth() {
-  if( typeof( window.innerWidth ) == 'number' ) {
-    return window.innerWidth;
-  } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
-    return document.documentElement.clientWidth;
-  } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
-    return document.body.clientWidth;
-  }
-}
-
-function getHeight() {
-  if( typeof( window.innerWidth ) == 'number' ) {
-    return window.innerHeight;
-  } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
-    return document.documentElement.clientHeight;
-  } else if( document.body && ( document.body.clientHeight || document.body.clientHeight ) ) {
-    return document.body.clientHeight;
-  }
-}
