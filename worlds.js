73c21b3768298876485c7888fd5b99a059744660
Natasha Hull-Richter <nhullrichter@blurb.com>
2014-08-23 14:24:11 -0700

    Start adding documentation and setup the game in the script tag

--- worlds.js
+++ worlds.js
@@ -2,6 +2,8 @@ var World = function(attrs) {
   this.attrs = attrs;
 };
 
+//Takes in a different world as a parameter
+//Averages the attributes of both worlds
 World.prototype.combine = function(otherWorld) {
   for(var attr in attrs) {
     this[attr] = avg(this[attr], otherWorld[attr]);
