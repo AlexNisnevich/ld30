267372038e9ceb0745d9b6a26b91cd6b7869be3a
Natasha Hull-Richter <nhullrichter@blurb.com>
2014-08-24 00:56:53 -0700

    Refactored object classes

---
+++ js/object_classes/base_classes.js
@@ -0,0 +1,72 @@
+// Example params
+// attrs = {
+//   x: 10,
+//   y: 15,
+//   length: 20,
+//   img: 'img/thinger1.png'
+// }
+var Thinger = function(attrs) {
+  this.length = attrs.length;
+
+  this.image = new createjs.Bitmap(attrs.img);
+  this.image.obj = this;
+  this.image.x = attrs.x;
+  this.image.y = attrs.y;
+
+  this.draw = function (game) {
+    this.image.scaleX = attrs.length / this.image.getBounds().width;
+    game.addObject(this.image);
+  };
+};
+
+// Example params
+// attrs = {
+//   x: 10,
+//   y: 15,
+//   length: 2,
+//   height: 20,
+//   img: 'img/tall_thinger1.png'
+// }
+var TallThinger = function(attrs) {
+  Thinger.apply(this, [attrs]);
+  this.height = attrs.height;
+  var _draw = this.draw.bind(this);
+
+  this.draw = function() {
+    this.image.scaleY = attrs.height / this.image.getBounds().height;
+    _draw();
+  };
+};
+
+// Makes player die
+var DeadlyThinger = function(attrs) {
+  Thinger.apply(this, [attrs]);
+  this.effectOnPlayer = "kill";
+};
+
+// Player bounces off of object
+var BounceThinger = function(attrs) {
+  Thinger.apply(this, [attrs]);
+  this.effectOnPlayer = "bounce";
+};
+
+// Makes player stop moving
+var Tree = function(attrs) {
+  TallThinger.apply(this, [attrs]);
+  this.effectOnPlayer = "stop";
+};
+
+// Makes player move faster
+var Ice = function(attrs) {
+  Thinger.apply(this, [attrs]);
+  this.effectOnPlayer = "speedUp";
+  this.shatterVelocity = 15;
+
+  this.shatter = function () {
+    this.image.scaleX = 0;
+  };
+
+  this.reset = function () {
+    this.image.scaleX = attrs.length / this.image.getBounds().width;
+  };
+};
