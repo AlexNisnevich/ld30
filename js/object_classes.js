9caec0dc148a4ecdb4aaab9b24c61fec535bcb86
Natasha Hull-Richter <nhullrichter@blurb.com>
2014-08-24 00:30:56 -0700

    Adding beehive

--- js/object_classes.js
+++ js/object_classes.js
@@ -100,6 +100,53 @@ var Tree = function(attrs) {
 // attrs = {
 //   x: 10,
 //   y: 15,
+//   radius: 5,
+//   img: 'img/platform1.png',
+//   maxRadius: 10,
+//   speed: 2
+// }
+var MovingBeeHive = function(attrs) {
+  // Until radius stuff is implemented
+  attrs.length = attrs.radius;
+  DeadlyThinger.apply(this, [attrs]);
+  this.radius = attrs.radius;
+  this.minRadius = attrs.radius;
+  this.maxRadius = attrs.maxRadius;
+  // Alex implement draw thingy here
+
+  this.move = function(playerPos) {
+    function distanceFromPlayer() {
+      return _euclideanDistance({x: this.image.x, y: this.image.y}, playerPos);
+    }
+
+    if(distanceFromPlayer < this.maxRadius) {
+      this.moveOutward();
+    } else {
+      this.moveInward();
+    }
+  };
+
+  this.moveOutward = function() {
+    if(this.maxRadius < this.radius) {
+      this.radius += this.speed;
+      // Until radius stuff is implemented
+      this.length = this.radius;
+    }
+  };
+
+  this.moveInward = function() {
+    if(this.minRadius > this.radius) {
+      this.radius -= this.speed;
+      // Until radius stuff is implemented
+      this.length = this.radius;
+    }
+  };
+};
+
+// Example params
+// attrs = {
+//   x: 10,
+//   y: 15,
 //   length: 20,
 //   img: 'img/platform1.png',
 //   distance: 15,
@@ -109,10 +156,8 @@ var MovingZombie = function(attrs) {
   DeadlyThinger.apply(this, [attrs]);
 
   this.move = function(playerPos) {
-    function distanceFromPlayer(playerPos) {
-      xDistance = Math.abs(playerPos.x - this.image.x);
-      yDistance = Math.abs(playerPos.y - this.image.y);
-      return xDistance + yDistance;
+    function distanceFromPlayer() {
+      return _euclideanDistance({x: this.image.x, y: this.image.y}, playerPos);
     }
 
     if(distanceFromPlayer(playerPos) < this.distance) {
@@ -186,3 +231,10 @@ MovingPlatform.prototype.move = function() {
       break;
   }
 };
+
+var _euclideanDistance = function (pos1, pos2) {
+  xDistance = Math.abs(pos1.x - pos2.x);
+  yDistance = Math.abs(pos1.y - pos2.y);
+  return Math.sqrt(Math.pow(xDistance,2) + Math.sqrt(yDistance,2));
+};
+
