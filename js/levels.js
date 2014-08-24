c66ec40b733fb69c96284291e34150ecbb32f46c
Alex Nisnevich <alex.nisnevich@gmail.com>
2014-08-24 00:23:50 -0700

    got sort of a low-gravity space level

--- js/levels.js
+++ js/levels.js
@@ -31,7 +31,7 @@ var LevelTwo = new World({
 }]);
 
 var LevelThree = new World({
-  name: "C",
+  name: "Ice1",
   start: [365, 270],
   goal: [775, 450],
   gravityCoefficient: 1
@@ -47,4 +47,23 @@ var LevelThree = new World({
   y: 465,
   length: 100,
   img: 'assets/ice.png'
+}]);
+
+var LevelFour = new World({
+  name: "Space1",
+  start: [925, 465],
+  goal: [95, 70],
+  gravityCoefficient: 0.2
+}, [{
+  type: "platform",
+  x: 45,
+  y: 45,
+  length: 135,
+  img: 'assets/platform.png'
+},{
+  type: "platform",
+  x: 900,
+  y: 495,
+  length: 80,
+  img: 'assets/platform.png'
 }]);
