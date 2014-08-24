5eed2441c6d8ee0cddda0145121c56268bac160e
Tikhon Jelvis <tikhon@jelv.is>
2014-08-23 18:54:48 -0700

    Replaced tabs with spaces for indentation.

--- js/game.js
+++ js/game.js
@@ -1,120 +1,120 @@
 var ShatteredWorlds = function() {
-	return new Game(1000, 600);
+  return new Game(1000, 600);
 };
 
 var Game = function(w, h) {
-	var assetsToLoad = {
-		'hero': 'assets/hero.png',
-		'platform': 'assets/platform.png',
-		'portal': 'assets/portal.jpg'
-	};
+  var assetsToLoad = {
+    'hero': 'assets/hero.png',
+    'platform': 'assets/platform.png',
+    'portal': 'assets/portal.jpg'
+  };
 
-	var self = this,
-		ticks = 0,
-		canvas,
-		stage,
-		container,
-		player,
-		world,
-		assets = [],
-		keyDown = false;
+  var self = this,
+  ticks = 0,
+  canvas,
+  stage,
+  container,
+  player,
+  world,
+  assets = [],
+  keyDown = false;
 
-	// holds all collideable objects
-	var collideables = [];
-	this.getCollideables = function() { return collideables; };
+  // holds all collideable objects
+  var collideables = [];
+  this.getCollideables = function() { return collideables; };
 
-	// starts to load all the assets
-	this.preloadResources = function() {
-		var requestedAssets = 0,
-			loadedAssets = 0;
+  // starts to load all the assets
+  this.preloadResources = function() {
+    var requestedAssets = 0,
+    loadedAssets = 0;
 
-		// loads the assets and keeps track
-		// of how many assets where there to
-		// be loaded
-		loadImage = function(name, path) {
-			var img = new Image();
-			img.onload = onLoadedAsset;
-			img.src = path;
+    // loads the assets and keeps track
+    // of how many assets where there to
+    // be loaded
+    loadImage = function(name, path) {
+      var img = new Image();
+      img.onload = onLoadedAsset;
+      img.src = path;
 
-			assets[name] = img;
+      assets[name] = img;
 
-			++requestedAssets;
-		}
-		// each time an asset is loaded
-		// check if all assets are complete
-		// and initialize the game, if so
-		onLoadedAsset = function(e) {
-			++loadedAssets;
-			if ( loadedAssets == requestedAssets ) {
-				self.initializeGame();
-			}
-		}
+      ++requestedAssets;
+    }
+    // each time an asset is loaded
+    // check if all assets are complete
+    // and initialize the game, if so
+    onLoadedAsset = function(e) {
+      ++loadedAssets;
+      if ( loadedAssets == requestedAssets ) {
+        self.initializeGame();
+      }
+    }
 
-		for (asset in assetsToLoad) {
-			loadImage(asset, assetsToLoad[asset]);
-		}
-	};
+    for (asset in assetsToLoad) {
+      loadImage(asset, assetsToLoad[asset]);
+    }
+  };
 
-	this.initializeGame = function() {
-		// creating the canvas-element
-		canvas = document.createElement('canvas');
-		canvas.width = w;
-		canvas.height = h;
-		document.body.appendChild(canvas);
+  this.initializeGame = function() {
+    // creating the canvas-element
+    canvas = document.createElement('canvas');
+    canvas.width = w;
+    canvas.height = h;
+    document.body.appendChild(canvas);
 
-		// initializing the stage
-		stage = new createjs.Stage(canvas);
-		container = new createjs.Container();
-		stage.addChild(container);
+    // initializing the stage
+    stage = new createjs.Stage(canvas);
+    container = new createjs.Container();
+    stage.addChild(container);
 
-		world = levels[3];
-		this.loadLevel(world);
+    world = levels[3];
+    this.loadLevel(world);
 
-		// Setting the listeners
-		document.onkeydown = function (e) {
-			player.handleKeyDown(e.keyCode);
-		}
-		document.onkeyup = function (e) {
-			player.handleKeyUp(e.keyCode);
-		}
+    // Setting the listeners
+    document.onkeydown = function (e) {
+      player.handleKeyDown(e.keyCode);
+    }
+    document.onkeyup = function (e) {
+      player.handleKeyUp(e.keyCode);
+    }
 
-		createjs.Ticker.setFPS(30);
-		createjs.Ticker.addEventListener('tick', self.tick);
-	};
+    createjs.Ticker.setFPS(30);
+    createjs.Ticker.addEventListener('tick', self.tick);
+  };
 
-	this.tick = function(e) {
-		ticks++;
-		player.tick();
-		world.tick();
-		stage.update();
-	};
+  this.tick = function(e) {
+    ticks++;
+    player.tick();
+    world.tick();
+    stage.update();
+  };
 
-	this.loadLevel = function(world) {
-		// place exit
-		var exit = new createjs.Bitmap(assets['portal']);
-		exit.x = world.goal[0];
-		exit.y = world.goal[1];
-		exit.name = "exit";
-		container.addChild(exit);
-		collideables.push(exit);
+  this.loadLevel = function(world) {
+    // place exit
+    var exit = new createjs.Bitmap(assets['portal']);
+    exit.x = world.goal[0];
+    exit.y = world.goal[1];
+    exit.name = "exit";
+    container.addChild(exit);
+    collideables.push(exit);
 
-		// place player
-		player = new Player(assets['hero'], world.start[0], world.start[1], self);
-		container.addChild(player.image);
+    // place player
+    player = new Player(assets['hero'], world.start[0], world.start[1], self);
+    container.addChild(player.image);
 
-		// place objects
-		_.each(world.objects, function (obj) {
-			console.log(obj);
-			obj.draw(self);
-		});
-	}
+    // place objects
+    _.each(world.objects, function (obj) {
+      console.log(obj);
+      obj.draw(self);
+    });
+  }
 
-	this.addObject = function(obj) {
-		container.addChild(obj);
-		collideables.push(obj);
-	};
+  this.addObject = function(obj) {
+    container.addChild(obj);
+    collideables.push(obj);
+  };
 
-	this.setup = function () {
-		self.preloadResources();
-	};
+  this.setup = function () {
+    self.preloadResources();
+  };
 };
--- js/levels.js
+++ js/levels.js
@@ -1,56 +1,56 @@
 var LevelOne = new World({
-	start: [240, 500],
-  	goal: [20, 370],
-  	gravityCoefficient: 1
+  start: [240, 500],
+  goal: [20, 370],
+  gravityCoefficient: 1
 }, [{
-	type: "platform",
-	x: 30,
-	y: 490,
-	length: 90,
-	img: 'assets/platform.png'
+  type: "platform",
+  x: 30,
+  y: 490,
+  length: 90,
+  img: 'assets/platform.png'
 },{
-	type: "platform",
-	x: 150,
-	y: 560,
-	length: 140,
-	img: 'assets/platform.png'
+  type: "platform",
+  x: 150,
+  y: 560,
+  length: 140,
+  img: 'assets/platform.png'
 }]);
 
 var LevelTwo = new World({
-	start: [430, 265],
-  	goal: [65, 435],
-  	gravityCoefficient: 1
+  start: [430, 265],
+  goal: [65, 435],
+  gravityCoefficient: 1
 }, [{
-	type: "platform",
-	x: 435,
-	y: 315,
-	length: 35,
-	img: 'assets/platform.png'
+  type: "platform",
+  x: 435,
+  y: 315,
+  length: 35,
+  img: 'assets/platform.png'
 }]);
 
 var LevelThree = new World({
-	start: [710, 520],
-  	goal: [910, 520],
-  	gravityCoefficient: 1
+  start: [710, 520],
+  goal: [910, 520],
+  gravityCoefficient: 1
 }, [{
-	type: "ice",
-	x: 690,
-	y: 560,
-	length: 80,
-	img: 'assets/ice.png'
+  type: "ice",
+  x: 690,
+  y: 560,
+  length: 80,
+  img: 'assets/ice.png'
 },{
-	type: "ice",
-	x: 890,
-	y: 560,
-	length: 90,
-	img: 'assets/ice.png'
+  type: "ice",
+  x: 890,
+  y: 560,
+  length: 90,
+  img: 'assets/ice.png'
 }]);
 
 
 // LevelTwo.combine(LevelOne);
 
 var levels = {
-	1: LevelOne,
-	2: LevelTwo,
-	3: LevelThree
+  1: LevelOne,
+  2: LevelTwo,
+  3: LevelThree
 };
