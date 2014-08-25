600dedf443a54e43e2a73491804c2352c38d3536
Tikhon Jelvis <tikhon@jelv.is>
2014-08-24 17:27:13 -0700

    Added an extra platform to my demo "game".

--- js/physics-game.js
+++ js/physics-game.js
@@ -5,6 +5,12 @@ function go() {
       y           : 100,
       length      : 100,
       restitution : 0.1
+    }),
+    platform({
+      x           : 300,
+      y           : 150,
+      length      : 100,
+      restitution : 0.1
     })]
   });
 
